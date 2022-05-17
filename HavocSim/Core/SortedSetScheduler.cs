e98332ed11bfc47ec39ffdd3f48b16bd37741a16
Brian Swenson <bpswenson@gmail.com>
2022-05-16 22:01:58 -0400

    Simple Sorted Set scheduler to have a workign scheduler

---
+++ HavocSim/Core/SortedSetScheduler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HavocSim.Core
+{
+    public class SortedSetScheduler : IScheduler
+    {
+        SortedSetScheduler()
+        {
+            _events = new SortedSet<Event>();
+        }
+        public void Insert(Event ev)
+        {
+            _events.Add(ev);
+        }
+        public bool IsEmpty()
+        {
+            return _events.Count == 0;
+        }
+        public Event PeekNext()
+        {
+            return _events.First();
+        }
+        public Event RemoveNext()
+        {
+            Event ev = _events.First();
+            _events.Remove(ev);
+            return ev;
+        }
+        public void Remove(Event ev)
+        {
+            _events.Remove(ev);
+        }
+        private SortedSet<Event> _events;
+    }
+}
--- HavocSim/Program.cs
+++ HavocSim/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HavocSim.Core;
 
+Console.WriteLine("Hello!");
